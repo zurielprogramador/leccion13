c2f8091b71e5c10deae767b6637a3cad3271f690
zurielprogramador <argenis.canton08@gmail.com>
2023-11-12 21:06:24 -0500

    corregirvideo4

---
+++ 010-fireChat/src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-heroe',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './heroe.component.html',
+  styleUrl: './heroe.component.css'
+})
+export class HeroeComponent {
+
+}
